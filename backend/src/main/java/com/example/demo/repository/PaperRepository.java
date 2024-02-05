1eff8f25f3ca2e122327de77d4daae82addd9be4
Krishnendhu-123 <133999613+Krishnendhu-123@users.noreply.github.com>
2024-02-05 23:17:57 +0530

    added api documentation

--- backend/src/main/java/com/example/demo/repository/PaperRepository.java
+++ backend/src/main/java/com/example/demo/repository/PaperRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Paper;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Repository interface for accessing and managing paper entities in the database.
+ */
 public interface PaperRepository extends JpaRepository<Paper,Long>{
 
     
