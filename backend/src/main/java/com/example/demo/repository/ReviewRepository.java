f02e7ba301984b59c14e4674434639582e6369fa
Mithunraj M <97949956+Myth-64@users.noreply.github.com>
2024-03-18 17:52:46 +0530

    Added findAll and findByPaper for Reviews

--- backend/src/main/java/com/example/demo/repository/ReviewRepository.java
+++ backend/src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.model.Review;
 
 public interface ReviewRepository extends JpaRepository<Review,Long>{
     
+    @Query(value="SELECT * FROM reviews WHERE paper_id=?1",nativeQuery=true)
+    List<Review> findByPaper(Long paperId);
 }
