d8c2f5f08e4a4c304c6fdfd7ad0c59d33c3634fe
Mithunraj M <97949956+Myth-64@users.noreply.github.com>
2024-03-19 04:51:31 +0530

    Reverted removal of findByUsername for User class

--- backend/src/main/java/com/example/demo/restController/UserRestController.java
+++ backend/src/main/java/com/example/demo/restController/UserRestController.java
@@ -9,11 +9,13 @@ import com.example.demo.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("api/users")
@@ -33,6 +35,18 @@ public class UserRestController {
         }
         else{
             return ResponseEntity.ok(userList);
+        }
+    }
+
+    @Operation(summary = "findByUsername", description="Return the details of the user with the given username")
+    @GetMapping("/findByUsername")
+    public ResponseEntity<Optional<User>> findByUsername(@RequestParam String username){
+        Optional<User>user=userRepository.findByUsername(username);
+        if(user.isPresent()){
+            return ResponseEntity.ok(user);
+        }
+        else{
+            return ResponseEntity.noContent().build();
         }
     }
 }
