32071e069f9509fa3659fb4ede7b410b54cb3376
Mithunraj M <97949956+Myth-64@users.noreply.github.com>
2024-03-21 15:44:37 +0530

    Added endpoints for accepting and rejecting papers

--- backend/src/main/java/com/example/demo/requestClasses/StatusChangePaperId.java
+++ backend/src/main/java/com/example/demo/requestClasses/StatusChangePaperId.java
@@ -2,4 +2,12 @@ package com.example.demo.requestClasses;
 
 public class StatusChangePaperId {
     Long paperId;
+
+    public Long getPaperId(){
+        return this.paperId;
+    }
+
+    public void setPaperId(Long paperId){
+        this.paperId=paperId;
+    }
 }
